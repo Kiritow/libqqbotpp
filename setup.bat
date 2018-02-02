4a2fff6e8a6d7feb32acf4efadc0a408efd67f18
kiritow <1362050620@qq.com>
2018-02-03 01:48:13 +0800

    Update setup script

--- setup.bat
+++ setup.bat
@@ -1,8 +1,11 @@
 @echo off
-echo "LibQQBotpp setup script"
-echo "Written by Kiritow (https://github.com/kiritow)"
+echo LibQQBotpp setup script
+echo Written by Kiritow (https://github.com/kiritow)
 
-echo "Creating temp directory..."
+echo Creating temp directory...
 mkdir tmp
 
-echo "Setup finished."
+echo Copying required dll files...
+copy CurlWrapper\lib\libcurl.dll libcurl.dll
+
+echo Setup finished.
